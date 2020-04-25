bdb3b4d8901cb430183bf209c3fe222e987e1a82
DESKTOP-QGPN99S\Mike <mikedeff@gmail.com>
2020-04-25 10:38:58 -0400

    fixed arena XY issues

--- src/entities/arena.rs
+++ src/entities/arena.rs
@@ -13,6 +13,8 @@ use crate::components::{Hitbox, HitboxShape};
 
 
 pub fn initialise_arena_walls(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>,) {
+    let ARENA_UI_HEIGHT = ARENA_HEIGHT + UI_HEIGHT;
+
     let mut wall_transform = Transform::default();
     wall_transform.set_translation_xyz(0.0, UI_HEIGHT-1.0, 0.0);
     wall_transform.set_scale(Vector3::new(40.0, 1.0, 0.0));
@@ -33,7 +35,8 @@ pub fn initialise_arena_walls(world: &mut World, sprite_sheet_handle: Handle<Spr
     //central circle
     let mut circle_transform = Transform::default();
     let scale = 4.0;
-    circle_transform.set_translation_xyz(ARENA_WIDTH/2.0, ARENA_WIDTH/2.0, 0.0);
+    
+    circle_transform.set_translation_xyz(ARENA_WIDTH/2.0, ARENA_UI_HEIGHT/2.0, 0.0);
     circle_transform.set_scale(Vector3::new(scale, scale, 0.0));
 
     let circle_sprite_render = SpriteRender {
@@ -51,17 +54,15 @@ pub fn initialise_arena_walls(world: &mut World, sprite_sheet_handle: Handle<Spr
 
     //outer circles
     let spacing_factor = 5.0;
-    let height = ARENA_HEIGHT + UI_HEIGHT;
-
     let scale = 2.0;
 
     for idx in 0..4 {
         let (starting_x, starting_y) = match idx {
-            0 => (ARENA_WIDTH / spacing_factor, height / 2.0),
-            1 => (ARENA_WIDTH / 2.0, height / spacing_factor),
-            2 => (ARENA_WIDTH - (ARENA_WIDTH / spacing_factor), height / 2.0),
-            3 => (ARENA_WIDTH / 2.0, height - (height / spacing_factor)),
-            _ => (ARENA_WIDTH / spacing_factor, height / spacing_factor),
+            0 => (ARENA_WIDTH / spacing_factor, ARENA_UI_HEIGHT / 2.0),
+            1 => (ARENA_WIDTH / 2.0, ARENA_UI_HEIGHT / spacing_factor),
+            2 => (ARENA_WIDTH - (ARENA_WIDTH / spacing_factor), ARENA_UI_HEIGHT / 2.0),
+            3 => (ARENA_WIDTH / 2.0, ARENA_UI_HEIGHT - (ARENA_UI_HEIGHT / spacing_factor)),
+            _ => (ARENA_WIDTH / spacing_factor, ARENA_UI_HEIGHT / spacing_factor),
         };
 
         let mut circle_transform = Transform::default();
