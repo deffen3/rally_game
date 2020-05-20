08260dc52eded71c8d4a754849fe709017219f72
DESKTOP-QGPN99S\Mike <mikedeff@gmail.com>
2020-05-19 20:43:55 -0400

    setting starting weapons back to standard

--- src/menu.rs
+++ src/menu.rs
@@ -404,7 +404,7 @@ impl SimpleState for MainMenu {
                         game_mode_setup.points_to_win = 10;
                         game_mode_setup.stock_lives = -1;
                         game_mode_setup.checkpoint_count = 0;
-                        game_mode_setup.starter_weapon = WeaponNames::Missile;
+                        game_mode_setup.starter_weapon = WeaponNames::LaserDoubleGimballed;
                         game_mode_setup.random_weapon_spawns = true;
                         game_mode_setup.keep_picked_up_weapons = false;
                         self.init_base_rules = true;
@@ -414,7 +414,7 @@ impl SimpleState for MainMenu {
                         game_mode_setup.points_to_win = -1;
                         game_mode_setup.stock_lives = 5;
                         game_mode_setup.checkpoint_count = 0;
-                        game_mode_setup.starter_weapon = WeaponNames::Rockets;
+                        game_mode_setup.starter_weapon = WeaponNames::LaserDoubleGimballed;
                         game_mode_setup.random_weapon_spawns = true;
                         game_mode_setup.keep_picked_up_weapons = false;
                         self.init_base_rules = true;
@@ -424,7 +424,7 @@ impl SimpleState for MainMenu {
                         game_mode_setup.points_to_win = -1;
                         game_mode_setup.stock_lives = -1;
                         game_mode_setup.checkpoint_count = 0;
-                        game_mode_setup.starter_weapon = WeaponNames::Mine;
+                        game_mode_setup.starter_weapon = WeaponNames::LaserDoubleGimballed;
                         game_mode_setup.random_weapon_spawns = true;
                         game_mode_setup.keep_picked_up_weapons = false;
                         self.init_base_rules = true;
