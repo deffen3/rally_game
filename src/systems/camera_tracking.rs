f92c9253fa337e3997119a6a23f9ceb764e04471
DESKTOP-QGPN99S\Mike <mikedeff@gmail.com>
2020-06-28 16:48:17 -0400

    starting to work on giving camera some momentum for smoother movement

--- src/systems/camera_tracking.rs
+++ src/systems/camera_tracking.rs
@@ -1,9 +1,10 @@
 use amethyst::{
     core::{
+        Time,
         transform::{Transform},
     },
     derive::SystemDesc,
-    ecs::{System, Join, SystemData, WriteStorage, ReadStorage, ReadExpect},
+    ecs::{System, Join, SystemData, WriteStorage, ReadStorage, ReadExpect, Read},
     window::ScreenDimensions,
     renderer::{
         camera::{Camera, Projection},
@@ -16,6 +17,10 @@ use crate::components::{Vehicle, Player, VehicleState};
 use crate::rally::{ARENA_HEIGHT, ARENA_WIDTH};
 
 
+const CAMERA_ZOOM_RATE: f32 = 0.01;
+const CAMERA_TRANSLATE_MAX_RATE: f32 = 1.0;
+
+
 #[derive(SystemDesc)]
 pub struct CameraTrackingSystem;
 
@@ -24,6 +29,7 @@ impl<'s> System<'s> for CameraTrackingSystem {
         ReadStorage<'s, Player>,
         ReadStorage<'s, Vehicle>,
         WriteStorage<'s, Transform>,
+        Read<'s, Time>,
         WriteStorage<'s, Camera>,
         ReadExpect<'s, ScreenDimensions>,
     );
@@ -33,10 +39,13 @@ impl<'s> System<'s> for CameraTrackingSystem {
         players,
         vehicles,
         mut transforms,
+        time,
         mut cameras,
         screen_dimensions,
     ): Self::SystemData
     ) {
+        let dt = time.delta_seconds();
+
         let mut vehicle_xs = Vec::<f32>::new();
         let mut vehicle_ys = Vec::<f32>::new();
 
@@ -74,14 +83,10 @@ impl<'s> System<'s> for CameraTrackingSystem {
         vehicle_max_y = (vehicle_max_y + offset).min(ARENA_HEIGHT);
 
         for (camera, transform) in (&mut cameras, &mut transforms).join() {
-            let _camera_x = transform.translation().x;
-            let _camera_y = transform.translation().y;
+            
+            let aspect_ratio = screen_dimensions.aspect_ratio();
 
-            let _camera_projection = camera.projection();
-
-            //log::info!("{} {} {:?}", camera_x, camera_y, camera_projection);
-
-            //Standard Projection
+            //Standard full Arena Projection
             // camera.set_projection(Projection::orthographic(
             //     -ARENA_WIDTH/2.0,
             //     ARENA_WIDTH/2.0,
@@ -91,25 +96,65 @@ impl<'s> System<'s> for CameraTrackingSystem {
             //     20.0,
             // ));
 
-            let aspect_ratio = screen_dimensions.aspect_ratio();
+            let camera_projection = camera.projection().as_orthographic().unwrap();
+            let camera_left = camera_projection.left();
+            let camera_right = camera_projection.right();
+            let camera_top = camera_projection.top();
+            let camera_bottom = camera_projection.bottom();
+
+            let camera_target_x = vehicle_min_x + (vehicle_max_x - vehicle_min_x)/2.0;
+            let camera_target_y = vehicle_min_y + (vehicle_max_y - vehicle_min_y)/2.0;
 
             let x_delta = vehicle_max_x - vehicle_min_x;
             let y_delta = vehicle_max_y - vehicle_min_y;
 
+            //keep aspect ratio consistent
             let max_delta = (x_delta/aspect_ratio).max(y_delta);
 
+            let camera_target_left = -max_delta*aspect_ratio/2.0;
+            let camera_target_right = max_delta*aspect_ratio/2.0;
+            let camera_target_top = -max_delta/2.0;
+            let camera_target_bottom = max_delta/2.0;
+
+            let camera_new_left = camera_left + (camera_target_left - camera_left);
+            let camera_new_right = camera_right + (camera_target_right - camera_right);
+            let camera_new_top = camera_top + (camera_target_top - camera_top);
+            let camera_new_bottom = camera_bottom + (camera_target_bottom - camera_bottom);
+
             camera.set_projection(Projection::orthographic(
-                -max_delta*aspect_ratio/2.0,
-                max_delta*aspect_ratio/2.0,
-                -max_delta/2.0,
-                max_delta/2.0,
+                camera_new_left,
+                camera_new_right,
+                camera_new_top,
+                camera_new_bottom,
                 0.0,
                 20.0,
             ));
-            
 
-            transform.set_translation_x(vehicle_min_x + (vehicle_max_x - vehicle_min_x)/2.0);
-            transform.set_translation_y(vehicle_min_y + (vehicle_max_y - vehicle_min_y)/2.0);
+
+            let camera_x = transform.translation().x;
+            let camera_y = transform.translation().y;
+
+            //Standard full Arena translation
+            // transform.set_translation_x(ARENA_WIDTH/2.0);
+            // transform.set_translation_y(ARENA_HEIGHT/2.0);
+
+            let mut dx = (camera_target_x - camera_x).min(CAMERA_TRANSLATE_MAX_RATE).max(-CAMERA_TRANSLATE_MAX_RATE);
+            if dx.abs() <= 0.001 {
+                dx = 0.0;
+            }
+
+            let mut dy = (camera_target_y - camera_y).min(CAMERA_TRANSLATE_MAX_RATE).max(-CAMERA_TRANSLATE_MAX_RATE);
+            if dy.abs() <= 0.001 {
+                dy = 0.0;
+            }
+
+            //transform.set_translation_x(camera_x + dx*dt);
+            transform.set_translation_x(camera_target_x);
+            //transform.set_translation_y(camera_y + dy*dt);
+            transform.set_translation_y(camera_target_y);
+
+            //log::info!("{} {}", camera_x, camera_y);
+            //log::info!("{} {} {} {}", camera_left, camera_right, camera_top, camera_bottom);
         }
     }
 }
