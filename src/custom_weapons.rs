9f8e7963032477aa0b9ae529512aa3d4581d7c07
DESKTOP-QGPN99S\Mike <mikedeff@gmail.com>
2020-07-12 11:58:41 -0400

    starting to setup weapon rules menu

--- src/custom_weapons.rs
+++ src/custom_weapons.rs
@@ -1,12 +1,16 @@
 use amethyst::{
+    assets::Loader,
     ecs::prelude::Entity,
     input::{is_close_requested, is_key_down},
     prelude::*,
-    ui::{UiCreator, UiEvent, UiEventType, UiFinder},
+    ui::{Anchor, TtfFormat, UiCreator, UiEvent, UiEventType, UiFinder, UiText, UiTransform},
+    utils::removal::{exec_removal, Removal},
     winit::VirtualKeyCode,
 };
 
 use crate::menu::MainMenu;
+
+use crate::components::{WeaponNames, WeaponStoreResource};
 
 const BUTTON_BACK_TO_MENU: &str = "back_to_menu";
 
@@ -14,6 +18,9 @@ const BUTTON_BACK_TO_MENU: &str = "back_to_menu";
 pub struct CustomWeaponsMenu {
     ui_root: Option<Entity>,
     button_back_to_menu: Option<Entity>,
+
+    weapon_names_list_display: Vec<(WeaponNames, String)>,
+    text_weapon_names: Vec<Entity>,
 }
 
 impl SimpleState for CustomWeaponsMenu {
@@ -21,9 +28,68 @@ impl SimpleState for CustomWeaponsMenu {
         // create UI from prefab and save the reference.
         let world = data.world;
 
+        world.register::<Removal<u32>>();
+        world.register::<UiText>();
+
         self.ui_root = Some(
             world.exec(|mut creator: UiCreator<'_>| creator.create("ui/custom_weapons.ron", ())),
         );
+
+        let font = world.read_resource::<Loader>().load(
+            "font/square.ttf",
+            TtfFormat,
+            (),
+            &world.read_resource(),
+        );
+
+        {
+            //Build list of weapon display names
+            let fetched_weapon_store = world.fetch::<WeaponStoreResource>();
+
+            for weapon_name in fetched_weapon_store.selection_order.iter() {
+                let weapon_properties = fetched_weapon_store.properties.get(&weapon_name);
+
+                if let Some(weapon_properties) = weapon_properties {
+                    let weapon_display_name = weapon_properties.display_name.clone();
+
+                    self.weapon_names_list_display
+                        .push((*weapon_name, weapon_display_name));
+                }
+            }
+        }
+
+        for (idx, weapon_displays) in self.weapon_names_list_display.iter().enumerate() {
+            let (_weapon_name, weapon_display_name) = weapon_displays;
+
+            let icon_transform = UiTransform::new(
+                weapon_display_name.to_string(),
+                Anchor::TopLeft,
+                Anchor::TopLeft,
+                50.0,
+                -140.0 - ((idx as f32) * 25.0),
+                0.3,
+                350.0,
+                18.0,
+            );
+
+            let mut ui_text = UiText::new(
+                font.clone(),
+                weapon_display_name.to_string(),
+                [1., 1., 1., 1.],
+                18.,
+            );
+
+            ui_text.align = Anchor::MiddleLeft;
+
+            let weapon_display_entity = world
+                .create_entity()
+                .with(icon_transform)
+                .with(ui_text)
+                .with(Removal::new(0 as u32))
+                .build();
+
+            self.text_weapon_names.push(weapon_display_entity);
+        }
     }
 
     fn update(&mut self, state_data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
@@ -79,8 +145,12 @@ impl SimpleState for CustomWeaponsMenu {
                 .expect("Failed to remove CustomWeaponsMenu");
         }
 
+        exec_removal(&data.world.entities(), &data.world.read_storage(), 0 as u32);
+
         self.ui_root = None;
 
         self.button_back_to_menu = None;
+
+        self.text_weapon_names = Vec::new();
     }
 }
